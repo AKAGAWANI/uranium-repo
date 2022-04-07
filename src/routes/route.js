b7f03226425dc6a9ca65e89ca7f9236cdb5eed1d
AKAGAWANI <anilkagawani@gmail.com>
2022-04-07 18:13:26 +0530

    postman assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,6 +10,24 @@ router.get('/test-me', function (req, res) {
     console.log('These are the request query parameters: ', req.query)
     res.send('My first ever api!')
 });
+router.get('/all-candidates', function (req, res) {
+  let candName=['arjun','sohel','anil','pandey','ram','bhagat','yogi']
+  let candList=[]
+  for(i=0;i<candName.length;i++){
+      candList.push(candName[i])
+      
+  }res.send(candList)
+});
+router.get('/candidates', function (req, res) {
+  let candName=['arjun','yogi','anil','sohel','pandey','ram','bhagat']
+  let candListNew=[]
+  let n=req.query.count
+  for(i=0;i<n;i++){
+      candListNew.push(candName[i])
+      
+  }res.send(candListNew)
+
+});
 
 
 
