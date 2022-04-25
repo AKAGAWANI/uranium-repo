cf9b91952145e4765dab5b31797b5f3f5018c2aa
AKAGAWANI <anilkagawani@gmail.com>
2022-04-25 20:34:19 +0530

    change 1

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -38,6 +38,25 @@ let getDistricts = async function (req, res) {
     }
 }
 
+let getByDistrictId = async function (req, res) {
+    try {
+        let district_id = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${district_id} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${date}`
+            // url: "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=district_id&date=date"
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
 let getByPin = async function (req, res) {
     try {
         let pin = req.query.pincode
@@ -83,3 +102,4 @@ module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getByDistrictId = getByDistrictId
